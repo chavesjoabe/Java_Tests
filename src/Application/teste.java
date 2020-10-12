86126da596c8b2542e955552a0c877855b7b308c
Joabe <chavesjoabe1@gmail.com>
2020-10-12 02:07:52 -0300

    criando pasta e salvando txt

--- src/Application/teste.java
+++ src/Application/teste.java
@@ -2,37 +2,47 @@ package Application;
 
 import System_Entities.Product;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class teste {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         List<Product> list = new ArrayList<>();
 
-        String path = "C:\\Users\\bkp\\Desktop\\Joabe\\Desenvolvimentos\\JAVA\\in.txt";
-        try(BufferedReader br = new BufferedReader(new FileReader(path)))
-        {
+        String path = "/Users/joabechaves/Documents/Developments/Udemy/JAVA/";
+        try (BufferedReader br = new BufferedReader(new FileReader(path+"teste.csv"))) {
             String line = br.readLine();
-            while (line!=null){
+            while (line != null) {
                 String[] features = line.split(",");
-                list.add(new Product(features[0], Double.parseDouble(features[1]),Double.parseDouble(features[2])));
+                list.add(new Product(features[0], Double.parseDouble(features[1]), Double.parseDouble(features[2])));
                 line = br.readLine();
             }
             System.out.println(list);
-            int i = 0;
-            for (Product product:list) {
-                System.out.println(list.get(i).getName()+", "+list.get(i).totalValue());
-                i++;
-            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        System.out.println("Deseja exportar os dados do almoxarifado?  -- s/n");
+        String res = scanner.next();
+        if(res.equalsIgnoreCase("s")){
 
-        catch(IOException e){
-            System.out.println("Error: " + e.getMessage());
+        boolean success = new File(path+ "/out").mkdir();
+            System.out.println("pasta criada com sucesso!! ");
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path+"/out/summary.txt"))){
+            for (Product product : list){
+                bw.write(product.getName()+ ", " + product.totalValue());
+                bw.newLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Deu ruim");
+        }
+        }
+        else{
+            System.out.println("Obrigado!! ");
         }
     }
 }
--- src/Application/teste02.java
+++ src/Application/teste02.java
@@ -1,2 +1,21 @@
-package Application;public class teste02 {
+package Application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class teste02 {
+    public static void main(String[] args) {
+        String path = "/Users/joabechaves/Documents/Developments/Udemy/JAVA/";
+        String[] lines = new String[]{"Ola", "Mundo"};
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path+"out.txt"))){
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Unexpected error");
+        }
+    }
 }
